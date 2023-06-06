bd1f12e638918320ed1f3b10bcce269d20e8c0b7
HamsterStudent <hwanyamyu@gmail.com>
2023-06-06 23:55:32 +0900

    polymorphis

---
+++ polymorphis.ts
@@ -0,0 +1,22 @@
+interface SStorage<T> {
+  // key가 제한되지 않은 오브젝트를 정의하게 해 줌
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  private storage: SStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringStorage = new LocalStorage<string>();
