31d9bd882567e9a00444d70537ef5c642349b580
HamsterStudent <hwanyamyu@gmail.com>
2023-06-07 23:10:40 +0900

    polymotphis

--- polymorphis.ts
+++ polymorphis.ts
@@ -5,14 +5,14 @@ interface SStorage<T> {
 
 class LocalStorage<T> {
   private storage: SStorage<T> = {};
-  set(key: string, value: T) {
+  setItem(key: string, value: T) {
     this.storage[key] = value;
   }
-  remove(key: string) {
-    delete this.storage[key];
-  }
-  get(key: string): T {
+  getItem(key: string): T {
     return this.storage[key];
+  }
+  clearItem(key: string) {
+    delete this.storage[key];
   }
   clear() {
     this.storage = {};
